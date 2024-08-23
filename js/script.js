abe965a3970890ead79d8a207c845434a7356181
syiifahusna <syiifahusna@gmail.com>
2024-08-23 20:00:55 +0800

    apply new theme

--- js/script.js
+++ js/script.js
@@ -1,50 +1,20 @@
-window.addEventListener("scroll", function(){
-    header = document.getElementsByTagName("header")[0];
-    logo = document.getElementsByClassName("logo")[0];
+const draggableBox = document.getElementById('draggableBox');
 
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        if(header.className.includes(" header-fixed")){
-		}else{
-			header.className += "  header-fixed";
-            logo.className += " logo-bg";
-		}
-    } else {
-        if(header.className.includes(" header-fixed")){
-			header.className = header.className.replace(" header-fixed", "");
-            logo.className = logo.className.replace(" logo-bg", "");
-		}else{
-		}
-    }
-})
+draggableBox.addEventListener('mousedown', function(e) {
+  // Enable dragging
+  var offsetX = e.clientX - parseInt(window.getComputedStyle(this).left);
+  var offsetY = e.clientY - parseInt(window.getComputedStyle(this).top);
 
-img = document.getElementsByClassName('img');
-img[0].style.opacity = "1";
-window.addEventListener('scroll',function(){
-    for(i=0;i<img.length;i++){
-        let visible = elementInViewport(img[i]);
-        if(visible == true){
-            img[i].style.opacity = "1";
-        }
-    }
-})
+  function mouseMoveHandler(e) {
+    draggableBox.style.left = e.clientX - offsetX + 'px';
+    draggableBox.style.top = e.clientY - offsetY + 'px';
+  }
 
-function elementInViewport(el) {
-    var top = el.offsetTop;
-    var left = el.offsetLeft;
-    var width = el.offsetWidth;
-    var height = el.offsetHeight;
-  
-    while(el.offsetParent) {
-      el = el.offsetParent;
-      top += el.offsetTop;
-      left += el.offsetLeft;
-    }
-  
-    return (
-      top < (window.pageYOffset + window.innerHeight) &&
-      left < (window.pageXOffset + window.innerWidth) &&
-      (top + height) > window.pageYOffset &&
-      (left + width) > window.pageXOffset
-    );
-}
+  function reset() {
+    window.removeEventListener('mousemove', mouseMoveHandler);
+    window.removeEventListener('mouseup', reset);
+  }
 
+  window.addEventListener('mousemove', mouseMoveHandler);
+  window.addEventListener('mouseup', reset);
+});
